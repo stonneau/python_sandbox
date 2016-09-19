1eb856ff99214c8be2e9dc301045a7d64cfbb38e
Steve Tonneau <stonneau@axle.laas.fr>
2016-09-19 14:06:20 +0200

    [BUG FIX]: odd number for input vector u. TODO remove all uses of arange

--- src/init_guess.py
+++ src/init_guess.py
@@ -38,9 +38,11 @@ def initial_guess_naive(params):
  	#now find closest x*dt < t_mid, with x integer
  	t_mid = floor(t_mid /dt)*dt
  	#now just apply it for half the time, then negatively the rest
- 	acc = [el for _ in arange(0,t_mid,dt) for el in ddc] + [-el for _ in arange(t_mid,phases[-1]-__EPS,dt) for el in ddc ]
+ 	acc = [el for _ in arange(0,t_mid-__EPS,dt) for el in ddc] + [-el for _ in arange(t_mid,phases[-1]-__EPS,dt) for el in ddc ]
  	# now add 0 dL
- 	return acc + [0 for _ in range(0,3) for _ in arange(0,phases[-1]-__EPS,dt)]
+ 	res= acc + [0 for _ in range(0,len(acc))]
+	assert len(res)%2 == 0, "in initial_guess_naive, u is not par"		
+ 	return res
  	
 def __barycenter(points):
 	x = zeros(3)
@@ -73,9 +75,12 @@ def initial_guess_support(params):
  	#now find closest x*dt < t_mid, with x integer
  	t_mid = floor(t_mid /dt)*dt
  	#now just apply it for half the time, then negatively the rest
- 	acc = [el for _ in arange(0,t_mid,dt) for el in ddc1] + [-el for _ in arange(t_mid,phases[-1],dt) for el in ddc2 ]
+ 	acc = [el for _ in arange(0,t_mid-__EPS,dt) for el in ddc1] + [-el for _ in arange(t_mid,phases[-1]-__EPS,dt) for el in ddc2 ]
  	# now add 0 dL
- 	return acc + [0 for _ in range(0,3) for _ in arange(0,phases[-1]-__EPS,dt)]
+ 	#~ res = acc + [0 for _ in range(0,3) for _ in arange(0,phases[-1]-__EPS,dt)] 	
+ 	res = acc + [0 for _ in range(0,len(acc))]
+ 	assert len(res)%2 == 0, "in initial_guess_support, u is not par"
+ 	return res
  	
 ## initial guess
 # which tries to compute two straight lines, with one middle
@@ -98,9 +103,12 @@ def initial_guess_naive_noise(params):
  	#now find closest x*dt < t_mid, with x integer
  	t_mid = floor(t_mid /dt)*dt
  	#now just apply it for half the time, then negatively the rest
- 	acc = [el for _ in arange(0,t_mid,dt) for el in ddc] + [-el for _ in arange(t_mid,phases[-1]-__EPS,dt) for el in ddc ]
+ 	acc = [el for _ in arange(0,t_mid-__EPS,dt) for el in ddc] + [-el for _ in arange(t_mid,phases[-1]-__EPS,dt) for el in ddc ]
  	# now add 0 dL
- 	return acc + [0 for _ in range(0,3) for _ in arange(0,phases[-1]-__EPS,dt)]
+ 	#~ res = acc + [0 for _ in range(0,3) for _ in arange(0,phases[-1]-__EPS,dt)]
+ 	res = acc + [0 for _ in range(0,len(acc))]
+ 	assert len(res)%2 == 0, "in initial_guess_naive_noise, u is not par"
+ 	return res
  	
 ## initial guess
 # that simply compensates for the gravity
@@ -116,7 +124,7 @@ def initial_guess_naive_gravity_compensation(params):
 	gdt = params["dt"] * params["g"];
 	phases = params["t_init_phases"];
 	ddc = [0,0,gdt]
-	return [el for _ in arange(0,phases[-1]-__EPS,dt) for el in ddc]
-	#~ for i in arange(0,phases[-1],dt):
-		#~ naive_guess[3*i+2] = naive_guess[3*i+2] + gdt
-	#~ return naive_guess
+	res = [el for _ in arange(0,phases[-1]-__EPS,dt) for el in ddc]
+	res += [0 for _ in range(0,len(acc))]
+ 	assert len(res)%2 == 0, "in initial_guess_naive_gravity_compensation, u is not par"
+ 	return res
